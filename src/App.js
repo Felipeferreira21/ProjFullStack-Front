4c3d10c7c3d53f51c89765fb00f5f8915b593d50
felipeferreira21 <felipeferre26@hotmail.com>
2023-09-21 16:38:19 -0300

    implementando formik

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home.js';
+import CreatePosts from './pages/CreatePosts';
 
 function App() {
-  const[listOfPosts, setListOfPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setListOfPosts(response.data);
-    });
-  }, []);
-
   return (
     <div className="App">
-      {listOfPosts.map((value, key) => {
-        return (
-          <div className='post'>
-            <div className='title'> {value.title} </div>
-            <div className='body'>{value.postText}</div>
-            <div className='footer'>{value.username}</div>
-          </div>
-        );
-      })}
+      <Router>
+        <Link to="/createposts"> Create A Posts </Link>
+        <Link to="/"> Home </Link>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/createposts" exact component={CreatePosts}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
